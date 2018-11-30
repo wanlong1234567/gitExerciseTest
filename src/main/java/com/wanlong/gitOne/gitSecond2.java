4af911f4e51b5534add3d57b202db6b9d9da8301
360BUYAD\wanlong3 <wanlong3@jd.com>
2018-11-30 17:34:32 +0800

    第二个分支上的一些改变

---
+++ src/main/java/com/wanlong/gitOne/gitSecond2.java
@@ -0,0 +1,12 @@
+package com.wanlong.gitOne;
+
+/**
+ * @author wl
+ * @date
+ */
+public class gitSecond2 {
+    public static void main(String[] args) {
+        System.out.println("https://blog.csdn.net/point0mine/article/details/82230379");
+        System.out.println("这个网址链接是关于git的一些使用");
+    }
+}
