b1ea17d6f02a6360f9be00e6c6367d8372224020
360BUYAD\wanlong3 <wanlong3@jd.com>
2018-11-30 17:00:54 +0800

    gitTese1上改变

---
+++ src/main/java/com/wanlong/gitOne/gitFirsst1.java
@@ -0,0 +1,11 @@
+package com.wanlong.gitOne;
+
+/**
+ * @author wl
+ * @date
+ */
+public class gitFirsst1 {
+    public static void main(String[] args) {
+        System.out.println("这个是分支gitTest1上的发生改变");
+    }
+}
