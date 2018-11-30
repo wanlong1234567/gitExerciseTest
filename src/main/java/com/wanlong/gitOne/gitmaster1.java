9ffe92c69a4863f4ac774d372abb2873433079a0
360BUYAD\wanlong3 <wanlong3@jd.com>
2018-11-30 16:58:44 +0800

    master1上改变

---
+++ src/main/java/com/wanlong/gitOne/gitmaster1.java
@@ -0,0 +1,11 @@
+package com.wanlong.gitOne;
+
+/**
+ * @author wl
+ * @date
+ */
+public class gitmaster1 {
+    public static void main(String[] args) {
+        System.out.println("这个是master分支上的---master1");
+    }
+}
