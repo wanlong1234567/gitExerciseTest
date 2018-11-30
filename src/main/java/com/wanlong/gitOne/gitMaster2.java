e48d63bf4f400b3646fc8b8a142c088895b1bdf2
360BUYAD\wanlong3 <wanlong3@jd.com>
2018-11-30 17:40:34 +0800

    master分支上的改变

---
+++ src/main/java/com/wanlong/gitOne/gitMaster2.java
@@ -0,0 +1,11 @@
+package com.wanlong.gitOne;
+
+/**
+ * @author wl
+ * @date 2018/11/30
+ */
+public class gitMaster2 {
+    public static void main(String[] args) {
+        System.out.println("master分支上的第二套数据：");
+    }
+}
