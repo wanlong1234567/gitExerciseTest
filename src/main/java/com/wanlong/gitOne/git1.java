e1628ada6c156c7f2f418d026c8b1a0a1e5935db
360BUYAD\wanlong3 <wanlong3@jd.com>
2018-11-30 16:53:51 +0800

    test

---
+++ src/main/java/com/wanlong/gitOne/git1.java
@@ -0,0 +1,11 @@
+package com.wanlong.gitOne;
+
+/**
+ * @author wl
+ * @date
+ */
+public class git1 {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
---
+++ src/test/com/wanlong/gitOne/git1Test.java
@@ -0,0 +1,7 @@
+package com.wanlong.gitOne;
+import static org.junit.Assert.*;
+
+public class git1Test {
+
+
+}
