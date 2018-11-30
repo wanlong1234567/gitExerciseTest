9365977ede12430561f390ebb3eba2670db2fbfd
360BUYAD\wanlong3 <wanlong3@jd.com>
2018-11-30 17:20:57 +0800

    gitTese2上改变

---
+++ src/main/java/com/wanlong/gitOne/gitSecond.java
@@ -0,0 +1,11 @@
+package com.wanlong.gitOne;
+
+/**
+ * @author wl
+ * @date
+ */
+public class gitSecond {
+    public static void main(String[] args) {
+        System.out.println("gitTest2分支上的改变");
+    }
+}
